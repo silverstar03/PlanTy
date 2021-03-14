d1b4170730caafc5e9cecaa71b941fb747a1f02b
guddus326 <guddus326@naver.com>
2021-03-14 20:15:04 +0900

    하단 탭바

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,94 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MaterialApp(home: MyTabs()));
+
+// TabController 객체를 멤버로 만들어서 상태를 유지하기 때문에 StatefulWidget 클래스 사용
+class MyTabs extends StatefulWidget{
+  @override
+  MyTabsState createState() => MyTabsState();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+// SingleTickerProviderStateMixin 클래스는 애니메이션을 처리하기 위한 헬퍼 클래스
+// 상속에 포함시키지 않으면 탭바 컨트롤러를 생성할 수 없다.
+// mixin은 다중 상속에서 코드를 재사용하기 위한 한 가지 방법으로 with 키워드와 함께 사용
+class MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin {
+  // 컨트롤러는 TabBar와 TabBarView 객체를 생성할 때 직접 전달
+  TabController controller;
+
+  // 객체가 위젯 트리에 추가될 때 호출되는 함수. 즉, 그려지기 전에 탭바 컨트롤러 샛성.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  void initState(){
+    super.initState();
+
+    // SingleTickerProviderStateMixin를 상속 받아서
+    // vsync에 this 형태로 전달해야 애니메이션이 정상 처리된다.
+    controller = TabController(vsync: this, length: 3);
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+  // initState 함수의 반대.
+  // 위젯 트리에서 제거되기 전에 호출. 멤버로 갖고 있는 컨트롤러부터 제거.
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void dispose(){
+    controller.dispose();
+    super.dispose();
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: AppBar(title: Text('PlanTy')),
+      body: TabBarView(
+          controller: controller,                     // 컨트롤러 연결
+          children: [Red(), Green(), Blue()]
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      bottomNavigationBar: Container(
+        child: TabBar(
+            controller: controller,                 // 컨트롤러 연결
+            tabs: [
+              // 아이콘은 글자 수 같은 걸로 선택. 의미 없음. 제목에 들어간 색상은 중요.
+              Tab(icon: Icon(Icons.card_travel), text: '빨강'),
+              Tab(icon: Icon(Icons.donut_small), text: '초록',),
+              Tab(icon: Icon(Icons.table_chart), text: '파랑'),
+            ]
         ),
+        color: Colors.blueGrey,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+// Card 위젯 구현
+class Red extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return Card(color: Colors.red);
+  }
+}
+
+// Text 위젯 구현
+class Green extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      child: Center(
+      ),
+      color: Colors.green,
+      margin: EdgeInsets.all(6.0),
+    );
+  }
+}
+
+// Icon 위젯 구현
+class Blue extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      child: Center(
+      ),
+      color: Colors.blue,
+      margin: EdgeInsets.all(6.0),
+    );
+  }
+}
+
+
